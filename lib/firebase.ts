2c8e1e47d02709ac6db241e28cb5e9f07e1d4f7c
Rafael Carriel <rafaelcarriel.deo@gmail.com>
2026-09-17 14:08:38 -0300

    feat(auth): initialize Firebase Auth instance

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,5 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -13,3 +14,5 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = initializeFirestore(app, { ignoreUndefinedProperties: true });
+
+export const auth = getAuth(app);
